30928329b14bbcd20f3c77777cc1d987580616b5
MuliaHartawan <mulia.hartawan011@gmail.com>
2022-10-04 23:19:21 +0700

    implementation spinner for waiting fetch search

--- resources/views/livewire/search-drop-down.blade.php
+++ resources/views/livewire/search-drop-down.blade.php
@@ -1,7 +1,7 @@
 <div class="relative mt-3 md:mt-0">
-    <input type="text" wire:model="search"
+    <input type="text" wire:model.debounce.500ms="search"
         class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1
-    focus:outline-none focus:shadow-outline"
+        focus:outline-none focus:shadow-outline"
         placeholder="Search">
     <div class="absolute top-0">
         <svg class="fill-current mt-2 w-4 text-gray-500 ml-2" viewBox="0 0 24 24">
@@ -9,6 +9,9 @@
                 d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z" />
         </svg>
     </div>
+
+    <div wire:loading class="spinner top-0 right-0 mr-6 mt-4"></div>
+
     @if (strlen($search) >= 2)
         <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4 shadow-lg">
             @if ($searchResults->count() > 0)
