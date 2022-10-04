73c02acdb63fea8787d46c498578cf159dd84d86
MuliaHartawan <mulia.hartawan011@gmail.com>
2022-10-04 23:08:38 +0700

    create component livewire search dropdown

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,15 +35,7 @@
                 </li>
             </ul>
             <div class="flex flex-col md:flex-row items-center gap-4 lg:gap-0">
-                <div class="relative mt-3 md:mt-0">
-                    <input type="text"
-                        class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1
-                    focus:outline-none focus:shadow-outline"
-                        placeholder="Search">
-                </div>
-                <div class="absolute top-0">
-                    <svg class="fill-current mt-50 w-4 text-gray-500 mr-56 md:mr-0 md:mt-10 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
-                </div>
+                <livewire:search-drop-down />
                 <div class="md:ml-4 md:md-mt-0">
                     <a href="#">
                         <img src="/img/avatar.jpg" alt="avatar" class="rounded-full w-8 h-8">
